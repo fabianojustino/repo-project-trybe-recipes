1d8c7e5d95462cb36d4fb69efaa2d37a1407d602
fabiano.justino <fibianojustino@gmail.com>
2022-05-28 16:30:50 -0300

    removendo componentes de class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import store from './store';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import mealReducer from './mealReducer';
-
-const rootReducer = combineReducers({ mealReducer });
-
-export default rootReducer;
